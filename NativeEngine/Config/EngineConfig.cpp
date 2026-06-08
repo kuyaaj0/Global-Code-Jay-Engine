537b25b58605e19637f5ee986b221bc2d40b91ec
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-08 21:18:55 +0800

    Engine settings configs

---
+++ NativeEngine/Config/EngineConfig.cpp
@@ -0,0 +1,24 @@
+#include "EngineConfig.hpp"
+
+// Engine
+int EngineConfig::engine = 0;
+
+// Gameplay
+bool EngineConfig::downscroll = false;
+bool EngineConfig::middlescroll = false;
+float EngineConfig::scrollSpeed = 2.0f;
+int EngineConfig::safeFrames = 10;
+
+// Performance
+bool EngineConfig::performanceMode = false;
+bool EngineConfig::lowEndMode = false;
+bool EngineConfig::matrix4Pipeline = false;
+
+// Modcharts
+bool EngineConfig::luaModcharts = false;
+bool EngineConfig::enable3D = false;
+bool EngineConfig::perspectiveCamera = false;
+bool EngineConfig::matrixTransforms = false;
+bool EngineConfig::optimizeLua = false;
+bool EngineConfig::notitgEffects = false;
+bool EngineConfig::depthSorting = false;

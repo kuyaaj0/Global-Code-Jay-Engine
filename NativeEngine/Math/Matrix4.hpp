80825234029db97f8693bda14e4bd58702939e89
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-10 06:37:23 +0800

    Looks like it's change the way of the matrix4 hpp

--- NativeEngine/Math/Matrix4.hpp
+++ NativeEngine/Math/Matrix4.hpp
@@ -1,28 +1,16 @@
 #pragma once
 
-#include "Vector3.hpp"
-
 class Matrix4
 {
+
 public:
 
-    float m[16];
+    float m[4][4];
 
     Matrix4();
 
-    static Matrix4 identity();
+    void identity();
 
-    static Matrix4 translation(float x, float y, float z);
+    static Matrix4 createIdentity();
 
-    static Matrix4 scale(float x, float y, float z);
-
-    static Matrix4 rotationX(float angle);
-
-    static Matrix4 rotationY(float angle);
-
-    static Matrix4 rotationZ(float angle);
-
-    Matrix4 operator*(const Matrix4& other) const;
-
-    Vector3 transformPoint(const Vector3& point) const;
 };

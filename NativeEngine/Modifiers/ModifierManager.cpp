ee1242a13695661e6d58e999ecf00cad0abd9f11
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-10 06:24:54 +0800

    Adding this part for applying it

--- NativeEngine/Modifiers/ModifierManager.cpp
+++ NativeEngine/Modifiers/ModifierManager.cpp
@@ -2,6 +2,27 @@
 
 ModifierManager::ModifierManager()
 {
+   // ModifierManager manager;
+
+manager.addModifier(new Reverse());
+manager.addModifier(new Drunk());
+manager.addModifier(new Tipsy());
+manager.addModifier(new Beat());
+manager.addModifier(new Confusion());
+
+manager.addModifier(new Tornado());
+manager.addModifier(new ZigZag());
+manager.addModifier(new Bounce());
+
+manager.addModifier(new Mini());
+manager.addModifier(new Hidden());
+manager.addModifier(new Sudden());
+
+manager.addModifier(new Boost());
+manager.addModifier(new Brake());
+manager.addModifier(new Wave());
+manager.addModifier(new Flip());
+manager.addModifier(new Invert());
 }
 
 void ModifierManager::addModifier(

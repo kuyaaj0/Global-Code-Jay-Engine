2d7a562e9cc9c2ffd7bd14e03eeccf3b1e8ddf5f
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 05:15:28 +0800

    That should update the camera pov math

--- NativeEngine/Renderer/Camera3D.hpp
+++ NativeEngine/Renderer/Camera3D.hpp
@@ -1,16 +1,48 @@
 #pragma once
 
-#include "../Math/Vector3.hpp"
+#include "../Core/Transform.hpp"
 #include "../Math/Matrix4.hpp"
+#include "../Math/Vector3.hpp"
 
 class Camera3D
 {
 public:
 
-    Vector3 position;
-    Vector3 rotation;
+    Transform transform;
+
+    float fov;
+    float nearPlane;
+    float farPlane;
+    float aspectRatio;
+
+    Matrix4 viewMatrix;
+    Matrix4 projectionMatrix;
 
     Camera3D();
 
-    Matrix4 getViewMatrix() const;
+    void Update();
+
+    void UpdateView();
+
+    void UpdateProjection();
+
+    void SetAspectRatio(
+        float aspect
+    );
+
+    void SetFOV(
+        float value
+    );
+
+    void Move(
+        const Vector3& delta
+    );
+
+    void LookAt(
+        const Vector3& target
+    );
+
+    Matrix4 GetViewMatrix() const;
+
+    Matrix4 GetProjectionMatrix() const;
 };

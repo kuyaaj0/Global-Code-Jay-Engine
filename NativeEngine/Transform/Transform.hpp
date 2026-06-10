9b62a2bcdf57723766ca0ccf3f3d882c36ac3654
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 05:12:16 +0800

    Hmm upgrade one?

--- NativeEngine/Transform/Transform.hpp
+++ NativeEngine/Transform/Transform.hpp
@@ -6,33 +6,53 @@
 
 class Transform
 {
-
 public:
 
+    // Position
     Vector3 position;
 
+    // Rotation
     Quaternion rotation;
 
+    // Scale
     Vector3 scale;
 
+    // Cached matrices
     Matrix4 localMatrix;
-
     Matrix4 worldMatrix;
 
+    // Constructor
     Transform();
 
-    void UpdateMatrix();
+    // Update matrices
+    void Update();
+
+    // Position
+    void SetPosition(
+        const Vector3& pos
+    );
 
     void Translate(
         const Vector3& delta
+    );
+
+    // Rotation
+    void SetRotation(
+        const Quaternion& rot
     );
 
     void Rotate(
         const Quaternion& delta
     );
 
+    // Scale
     void SetScale(
-        const Vector3& newScale
+        const Vector3& scl
     );
 
+    // Reset
+    void Reset();
+
+    // Get final transform matrix
+    Matrix4 GetMatrix() const;
 };

7285fd0d024ead8b264672dde788ce8763b88a09
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-10 19:32:42 +0800

    Updated this one to extend a bit

--- NativeEngine/Math/Matrix4.hpp
+++ NativeEngine/Math/Matrix4.hpp
@@ -1,16 +1,39 @@
 #pragma once
 
+#include "Vector3.hpp"
+#include "Vector4.hpp"
+
 class Matrix4
 {
-
 public:
 
     float m[4][4];
 
     Matrix4();
 
-    void identity();
+    void Identity();
 
-    static Matrix4 createIdentity();
+    static Matrix4 IdentityMatrix();
 
+    static Matrix4 Translation(
+        float x,
+        float y,
+        float z
+    );
+
+    static Matrix4 Scale(
+        float x,
+        float y,
+        float z
+    );
+
+    static Matrix4 RotationX(float angle);
+
+    static Matrix4 RotationY(float angle);
+
+    static Matrix4 RotationZ(float angle);
+
+    Matrix4 operator*(const Matrix4& other) const;
+
+    Vector4 Transform(const Vector4& vec) const;
 };

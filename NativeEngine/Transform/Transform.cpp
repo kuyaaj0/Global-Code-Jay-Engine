3975de23b09dfa3be9c00697650fd65347e35046
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-10 19:43:46 +0800

    There you go

--- NativeEngine/Transform/Transform.cpp
+++ NativeEngine/Transform/Transform.cpp
@@ -2,9 +2,66 @@
 
 Transform::Transform()
 {
-    position = Vector3(0.0f,0.0f,0.0f);
+    position = Vector3(0,0,0);
 
-    rotation = Vector3(0.0f,0.0f,0.0f);
+    rotation = Quaternion::Identity();
 
-    scale = Vector3(1.0f,1.0f,1.0f);
+    scale = Vector3(1,1,1);
+
+    UpdateMatrix();
+}
+
+void Transform::UpdateMatrix()
+{
+    Matrix4 T =
+        Matrix4::Translation(
+            position.x,
+            position.y,
+            position.z
+        );
+
+    Matrix4 R =
+        rotation.ToMatrix();
+
+    Matrix4 S =
+        Matrix4::Scale(
+            scale.x,
+            scale.y,
+            scale.z
+        );
+
+    localMatrix =
+        T * R * S;
+
+    worldMatrix =
+        localMatrix;
+}
+
+void Transform::Translate(
+const Vector3& delta)
+{
+    position =
+        position + delta;
+
+    UpdateMatrix();
+}
+
+void Transform::Rotate(
+const Quaternion& delta)
+{
+    rotation =
+        rotation * delta;
+
+    rotation.Normalize();
+
+    UpdateMatrix();
+}
+
+void Transform::SetScale(
+const Vector3& newScale)
+{
+    scale =
+        newScale;
+
+    UpdateMatrix();
 }

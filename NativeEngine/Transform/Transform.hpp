a50847357f0ec8714a9054d0526a17a4a5376e9c
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-10 19:43:10 +0800

    That should do I guess

--- NativeEngine/Transform/Transform.hpp
+++ NativeEngine/Transform/Transform.hpp
@@ -1,16 +1,38 @@
 #pragma once
 
 #include "../Math/Vector3.hpp"
+#include "../Math/Quaternion.hpp"
+#include "../Math/Matrix4.hpp"
 
 class Transform
 {
+
 public:
 
     Vector3 position;
 
-    Vector3 rotation;
+    Quaternion rotation;
 
     Vector3 scale;
 
+    Matrix4 localMatrix;
+
+    Matrix4 worldMatrix;
+
     Transform();
+
+    void UpdateMatrix();
+
+    void Translate(
+        const Vector3& delta
+    );
+
+    void Rotate(
+        const Quaternion& delta
+    );
+
+    void SetScale(
+        const Vector3& newScale
+    );
+
 };

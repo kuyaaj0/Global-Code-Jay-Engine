7eaad52d92572a361b7df695d755d5865c9d59bc
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-10 11:16:24 +0800

    Hmm I guess that's good amount of the code lines

--- NativeEngine/Math/Vector2.cpp
+++ NativeEngine/Math/Vector2.cpp
@@ -1,4 +1,5 @@
 #include "Vector2.hpp"
+#include <cmath>
 
 Vector2::Vector2()
 {
@@ -6,8 +7,85 @@ Vector2::Vector2()
     y = 0.0f;
 }
 
-Vector2::Vector2(float xValue, float yValue)
+Vector2::Vector2(float X,float Y)
 {
-    x = xValue;
-    y = yValue;
+    x = X;
+    y = Y;
+}
+
+Vector2 Vector2::operator+(const Vector2& other) const
+{
+    return Vector2(
+        x + other.x,
+        y + other.y
+    );
+}
+
+Vector2 Vector2::operator-(const Vector2& other) const
+{
+    return Vector2(
+        x - other.x,
+        y - other.y
+    );
+}
+
+Vector2 Vector2::operator*(float scalar) const
+{
+    return Vector2(
+        x * scalar,
+        y * scalar
+    );
+}
+
+Vector2 Vector2::operator/(float scalar) const
+{
+    return Vector2(
+        x / scalar,
+        y / scalar
+    );
+}
+
+float Vector2::Length() const
+{
+    return std::sqrt(x*x + y*y);
+}
+
+float Vector2::LengthSquared() const
+{
+    return x*x + y*y;
+}
+
+Vector2 Vector2::Normalized() const
+{
+    float len = Length();
+
+    if(len == 0.0f)
+        return Vector2();
+
+    return Vector2(
+        x/len,
+        y/len
+    );
+}
+
+float Vector2::Dot(
+    const Vector2& a,
+    const Vector2& b)
+{
+    return a.x*b.x + a.y*b.y;
+}
+
+float Vector2::Distance(
+    const Vector2& a,
+    const Vector2& b)
+{
+    return (a-b).Length();
+}
+
+Vector2 Vector2::Lerp(
+    const Vector2& a,
+    const Vector2& b,
+    float t)
+{
+    return a + (b-a)*t;
 }

5735822e6c76a977f73fb73a0b27c54220f983b1
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 16:08:08 +0800

    Create HUD.cpp

---
+++ NativeEngine/Hud/HUD.cpp
@@ -0,0 +1,19 @@
+#include "HUD.hpp"
+
+void HUD::Update()
+{
+    // Future:
+    // Update score
+    // Update combo
+    // Update health
+    // Update accuracy
+}
+
+void HUD::Render()
+{
+    // Future:
+    // Draw health bar
+    // Draw score
+    // Draw combo
+    // Draw accuracy
+}

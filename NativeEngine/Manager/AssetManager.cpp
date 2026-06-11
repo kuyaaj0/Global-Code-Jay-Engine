36b5692465880138978f64036696fb3183b68e99
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 20:53:38 +0800

    Create AssetManager.cpp

---
+++ NativeEngine/Manager/AssetManager.cpp
@@ -0,0 +1,44 @@
+#include "AssetManager.hpp"
+
+AssetManager::AssetManager()
+{
+
+}
+
+bool AssetManager::LoadTexture(
+const std::string& name,
+const std::string& path)
+{
+    textures[name] = path;
+    return true;
+}
+
+bool AssetManager::LoadSound(
+const std::string& name,
+const std::string& path)
+{
+    sounds[name] = path;
+    return true;
+}
+
+bool AssetManager::LoadFont(
+const std::string& name,
+const std::string& path)
+{
+    fonts[name] = path;
+    return true;
+}
+
+bool AssetManager::HasTexture(
+const std::string& name) const
+{
+    return textures.find(name)
+        != textures.end();
+}
+
+void AssetManager::Clear()
+{
+    textures.clear();
+    sounds.clear();
+    fonts.clear();
+}

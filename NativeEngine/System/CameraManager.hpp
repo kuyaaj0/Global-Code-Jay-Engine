c5a59ba445cfbe864777a2b9cf2399fce09b9911
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 21:22:23 +0800

    Probably this would be the left and right turn camera angle

---
+++ NativeEngine/System/CameraManager.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../Math/Vector3.hpp"
+
+class CameraManager
+{
+
+public:
+
+    CameraManager();
+
+    void SetPosition(
+        const Vector3& pos
+    );
+
+    void Move(
+        const Vector3& delta
+    );
+
+    void SetZoom(
+        float value
+    );
+
+    Vector3 GetPosition() const;
+
+    float GetZoom() const;
+
+private:
+
+    Vector3 position;
+
+    float zoom;
+
+};

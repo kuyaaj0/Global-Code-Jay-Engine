d0a142da2956584c88205b110ea574ca085b1e80
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 15:51:07 +0800

    Create EventSystem.cpp

---
+++ NativeEngine/States/Event/EventSystem.cpp
@@ -0,0 +1,38 @@
+#include "EventSystem.hpp"
+
+EventSystem::EventSystem()
+{
+
+}
+
+void EventSystem::AddEvent(
+const GameEvent& event)
+{
+    events.push_back(event);
+}
+
+void EventSystem::Update(
+float songPosition)
+{
+    for(auto& event : events)
+    {
+        if(event.executed)
+            continue;
+
+        if(songPosition >= event.time)
+        {
+            event.executed = true;
+
+            // Future:
+            // Call Lua
+            // Camera Events
+            // Character Events
+            // Shader Events
+        }
+    }
+}
+
+void EventSystem::Clear()
+{
+    events.clear();
+}

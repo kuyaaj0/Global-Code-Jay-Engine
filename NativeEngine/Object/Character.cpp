5911a45abb6b2e37cecd57aaaf921baf2590b5dd
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 16:28:31 +0800

    Soon

---
+++ NativeEngine/Object/Character.cpp
@@ -0,0 +1,48 @@
+#include "Character.hpp"
+
+Character::Character()
+{
+    alpha = 1.0f;
+
+    visible = true;
+
+    flipX = false;
+
+    flipY = false;
+}
+
+void Character::Idle()
+{
+    // TODO:
+    // Play idle animation
+}
+
+void Character::SingLeft()
+{
+
+}
+
+void Character::SingDown()
+{
+
+}
+
+void Character::SingUp()
+{
+
+}
+
+void Character::SingRight()
+{
+
+}
+
+void Character::Miss()
+{
+
+}
+
+void Character::Update(float dt)
+{
+
+}

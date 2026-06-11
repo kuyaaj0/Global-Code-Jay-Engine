379470bc2768a439435302337079a79e00baf23d
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 16:02:37 +0800

    Implemented the HUD overlay

---
+++ NativeEngine/Hud/HUD.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+class HUD
+{
+
+public:
+
+    float health;
+
+    int score;
+
+    int combo;
+
+    int misses;
+
+    float accuracy;
+
+    void Update();
+
+    void Render();
+
+};

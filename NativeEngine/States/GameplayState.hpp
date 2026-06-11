3524068c58e75638fec28cd0bdee67b14895e9ca
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 15:57:52 +0800

    Adding the gameplay state for running in game

---
+++ NativeEngine/States/GameplayState.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "../Gameplay/NoteManager.hpp"
+#include "../Modifier/ModifierManager.hpp"
+#include "../PlayField/PlayField.hpp"
+#include "../Gameplay/StrumLine.hpp"
+#include "ChartParser.hpp"
+
+#include "../Audio/AudioManager.hpp"
+#include "InputManager.hpp"
+#include "Events/EventSystem.hpp"
+
+#include "../Renderer/Renderer.hpp"
+
+class GameplayState
+{
+
+public:
+
+    GameplayState();
+
+    void Load();
+
+    void Update(
+        float dt
+    );
+
+    void Render();
+
+    void Reset();
+
+private:
+
+    Renderer renderer;
+
+    AudioManager audio;
+
+    InputManager input;
+
+    EventSystem events;
+
+    ChartParser parser;
+
+    NoteManager noteManager;
+
+    ModifierManager modifierManager;
+
+    PlayField playField;
+
+    StrumLine playerStrums[4];
+
+    StrumLine opponentStrums[4];
+
+};

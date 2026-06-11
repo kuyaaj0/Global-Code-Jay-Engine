ddab4dbd488b3b478f8b76f0167767d9770daef2
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 16:07:00 +0800

    Create Song loader.cpp

---
+++ NativeEngine/Loader/Song loader.cpp
@@ -0,0 +1,19 @@
+#include "SongLoader.hpp"
+
+bool SongLoader::LoadSong(
+const std::string& folder)
+{
+    return true;
+}
+
+bool SongLoader::LoadChart(
+const std::string& chart)
+{
+    return true;
+}
+
+bool SongLoader::LoadAudio(
+const std::string& audio)
+{
+    return true;
+}

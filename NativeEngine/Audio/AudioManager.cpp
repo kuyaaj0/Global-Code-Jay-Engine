6c9b60c670397e0717b1577e0ce2d3e0a0e75c73
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 15:53:28 +0800

    That should do

---
+++ NativeEngine/Audio/AudioManager.cpp
@@ -0,0 +1,55 @@
+#include "AudioManager.hpp"
+
+AudioManager::AudioManager()
+{
+    currentPosition = 0.0f;
+
+    playing = false;
+}
+
+bool AudioManager::LoadSong(
+const std::string& path)
+{
+    // Future:
+    // Decode OGG
+    // Decode MP3
+    // Decode WAV
+
+    currentPosition = 0.0f;
+
+    return true;
+}
+
+void AudioManager::Play()
+{
+    playing = true;
+}
+
+void AudioManager::Pause()
+{
+    playing = false;
+}
+
+void AudioManager::Stop()
+{
+    playing = false;
+
+    currentPosition = 0.0f;
+}
+
+void AudioManager::Seek(
+float milliseconds)
+{
+    currentPosition =
+        milliseconds;
+}
+
+float AudioManager::GetPosition() const
+{
+    return currentPosition;
+}
+
+bool AudioManager::IsPlaying() const
+{
+    return playing;
+}

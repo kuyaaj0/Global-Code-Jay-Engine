a9504bf9940002821fe840ce93d3c02c9a8ee151
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 16:29:23 +0800

    Add Stage class with Load, Update, and Render methods
    Implement Stage class with basic methods.

---
+++ NativeEngine/States/Stage/Stage.cpp
@@ -0,0 +1,27 @@
+#include "Stage.hpp"
+
+Stage::Stage()
+{
+    cameraZoom = 1.0f;
+
+    visible = true;
+}
+
+void Stage::Load()
+{
+    // TODO:
+    // Load background
+    // Load props
+    // Load foreground
+}
+
+void Stage::Update(float dt)
+{
+
+}
+
+void Stage::Render()
+{
+    // TODO:
+    // Render stage objects
+}

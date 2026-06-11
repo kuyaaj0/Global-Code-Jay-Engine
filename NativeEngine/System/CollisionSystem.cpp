494cdd414abf8fcd08a68dd88835a92ff84e4024
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 21:38:53 +0800

    This is some kind of matrix that collision to explode for a second

---
+++ NativeEngine/System/CollisionSystem.cpp
@@ -0,0 +1,24 @@
+#include "CollisionSystem.hpp"
+
+CollisionSystem::CollisionSystem()
+{
+
+}
+
+bool CollisionSystem::BoxCollision(
+float x1,
+float y1,
+float w1,
+float h1,
+float x2,
+float y2,
+float w2,
+float h2
+) const
+{
+    return
+        x1 < x2 + w2 &&
+        x1 + w1 > x2 &&
+        y1 < y2 + h2 &&
+        y1 + h1 > y2;
+}

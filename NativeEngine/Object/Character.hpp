ff2569065694da71a7357bb6a0f10ea59a9a1439
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 16:20:33 +0800

    Implemented character to make it interact

---
+++ NativeEngine/Object/Character.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../Transform/Transform.hpp"
+
+class Character
+{
+
+public:
+
+    Transform transform;
+
+    float alpha;
+
+    bool visible;
+
+    bool flipX;
+
+    bool flipY;
+
+    Character();
+
+    void Idle();
+
+    void SingLeft();
+
+    void SingDown();
+
+    void SingUp();
+
+    void SingRight();
+
+    void Miss();
+
+    void Update(float dt);
+
+};

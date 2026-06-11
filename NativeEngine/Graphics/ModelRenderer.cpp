787949a85dfadf3f498f1fc904676aed1d4d75cb
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-12 07:52:25 +0800

    I guess it's still soon

---
+++ NativeEngine/Graphics/ModelRenderer.cpp
@@ -0,0 +1,16 @@
+#include "ModelRenderer.hpp"
+
+ModelRenderer::ModelRenderer()
+{
+
+}
+
+void ModelRenderer::Draw(
+Model* model,
+Camera3D* camera)
+{
+    // Future:
+    // Bind shader
+    // Bind material
+    // Draw meshes
+}

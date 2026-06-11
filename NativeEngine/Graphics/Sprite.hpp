2e0a0ddfd87b409ad279174d567b24b503e95cd5
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-11 21:46:08 +0800

    Sprite or something?

---
+++ NativeEngine/Graphics/Sprite.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../Math/Vector3.hpp"
+
+class Texture;
+
+class Sprite
+{
+
+public:
+
+    Sprite();
+
+    Vector3 position;
+
+    Vector3 scale;
+
+    float rotation;
+
+    Texture* texture;
+
+    void Draw();
+
+};

a11f3afb5f161944940930db95f95b332d5465f0
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-12 08:27:33 +0800

    Adding the anim frame for characters I think

---
+++ NativeEngine/Animation/AnimationFrame.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+struct AnimationFrame
+{
+    int frameIndex;
+    float duration;
+};
+
+class Animation
+{
+
+public:
+
+    Animation();
+
+    void SetName(
+        const std::string& name
+    );
+
+    void AddFrame(
+        int frame,
+        float duration
+    );
+
+    const std::string& GetName() const;
+
+    const std::vector<AnimationFrame>& GetFrames() const;
+
+    void Clear();
+
+private:
+
+    std::string animationName;
+
+    std::vector<AnimationFrame> frames;
+
+};

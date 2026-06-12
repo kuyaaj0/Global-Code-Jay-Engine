f649e4c683a132a2f67c1c674696e791a1a0e7bc
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-12 08:34:27 +0800

    That should be enough now

---
+++ NativeEngine/Animation/ModelLoader.cpp
@@ -0,0 +1,19 @@
+#include "ModelLoader.hpp"
+#include "Model.hpp"
+
+ModelLoader::ModelLoader()
+{
+
+}
+
+bool ModelLoader::Load(
+const std::string& path,
+Model& model)
+{
+    // Future:
+    // Load OBJ
+    // Load FBX
+    // Load GLTF
+
+    return true;
+}

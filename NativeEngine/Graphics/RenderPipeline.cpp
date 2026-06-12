34ac6a214c1704cc2d213fbeb4d6888f84c6f96a
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-12 08:21:26 +0800

    I guess they're empty yet

---
+++ NativeEngine/Graphics/RenderPipeline.cpp
@@ -0,0 +1,31 @@
+#include "RenderPipeline.hpp"
+
+RenderPipeline::RenderPipeline()
+{
+
+}
+
+void RenderPipeline::BeginFrame()
+{
+
+}
+
+void RenderPipeline::GeometryPass()
+{
+
+}
+
+void RenderPipeline::LightingPass()
+{
+
+}
+
+void RenderPipeline::PostProcessPass()
+{
+
+}
+
+void RenderPipeline::EndFrame()
+{
+
+}

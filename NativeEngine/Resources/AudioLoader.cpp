4cdac4211cd28ca6ab12ffad4d2986d8fd9789c5
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-12 12:37:45 +0800

    Yea it is

---
+++ NativeEngine/Resources/AudioLoader.cpp
@@ -0,0 +1,15 @@
+#include "AudioLoader.hpp"
+
+AudioLoader::AudioLoader()
+{
+
+}
+
+bool AudioLoader::Load(
+const std::string& path)
+{
+    // Future:
+    // Load OGG/WAV/MP3
+
+    return true;
+}

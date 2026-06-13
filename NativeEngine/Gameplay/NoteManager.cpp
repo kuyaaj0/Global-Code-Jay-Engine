4d061c493d7097b2337df12cc29c4bae2c1b1158
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-14 05:47:33 +0800

    Ok

--- NativeEngine/Gameplay/NoteManager.cpp
+++ NativeEngine/Gameplay/NoteManager.cpp
@@ -2,21 +2,47 @@
 
 #include "Note3D.hpp"
 
+#include "../Modifier/ModifierManager.hpp"
+
+#include <cmath>
+
 NoteManager::NoteManager()
 {
 
+}
+
+NoteManager::~NoteManager()
+{
+    for(Note3D* note : notes)
+    {
+        delete note;
+    }
+
+    notes.clear();
+}
+
+void NoteManager::AddNote(
+Note3D* note)
+{
+    if(note != nullptr)
+    {
+        notes.push_back(note);
+    }
 }
 
 void NoteManager::Update(
 float songPosition,
 ModifierManager* modifiers)
 {
-    for(auto note : notes)
+    for(Note3D* note : notes)
     {
-        note->Update(
-            songPosition,
-            modifiers
-        );
+        if(note != nullptr)
+        {
+            note->Update(
+                songPosition,
+                modifiers
+            );
+        }
     }
 }
 
@@ -24,15 +50,48 @@ void NoteManager::Render()
 {
     for(Note3D* note : notes)
     {
-        note->Render();
+        if(note != nullptr)
+        {
+            note->Render();
+        }
     }
 }
 
-void NoteManager::MarkHit(
-NoteData* note)
+Note3D* NoteManager::FindClosestNote(
+int lane,
+float songPosition)
 {
-    if(note != nullptr)
+    Note3D* closest = nullptr;
+
+    float closestDistance = 9999999.0f;
+
+    for(Note3D* note : notes)
     {
-        note->hit = true;
+        if(note == nullptr)
+            continue;
+
+        // We'll compare lane/time later
+        // after upgrading Note3D.
+
+        float distance = 0.0f;
+
+        if(distance < closestDistance)
+        {
+            closestDistance = distance;
+
+            closest = note;
+        }
     }
+
+    return closest;
+}
+
+void NoteManager::MarkHit(
+Note3D* note)
+{
+    if(note == nullptr)
+        return;
+
+    // We'll implement this after
+    // adding hit state to Note3D.
 }

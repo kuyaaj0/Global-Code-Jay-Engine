4ec9ed0c59231a59133104b7b8c0ea0f01403528
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-13 09:20:59 +0800

    What is this bro

--- NativeEngine/States/InputManager.cpp
+++ NativeEngine/States/InputManager.cpp
@@ -2,41 +2,20 @@
 
 InputManager::InputManager()
 {
-    current.fill(false);
 
-    previous.fill(false);
 }
 
 void InputManager::Update()
 {
-    previous = current;
+
 }
 
-bool InputManager::IsPressed(
-int lane) const
+bool InputManager::IsLanePressed(
+int lane
+) const
 {
-    return current[lane];
-}
+    // Future:
+    // Keyboard/Touch input
 
-bool InputManager::IsJustPressed(
-int lane) const
-{
-    return
-        current[lane] &&
-        !previous[lane];
-}
-
-bool InputManager::IsJustReleased(
-int lane) const
-{
-    return
-        !current[lane] &&
-        previous[lane];
-}
-
-void InputManager::SetState(
-int lane,
-bool value)
-{
-    current[lane] = value;
+    return false;
 }

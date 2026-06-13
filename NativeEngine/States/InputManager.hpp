3fbfc3cfb0fc0f9723cd1e35458276cadfaf7746
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-13 09:20:00 +0800

    Wth bro

--- NativeEngine/States/InputManager.hpp
+++ NativeEngine/States/InputManager.hpp
@@ -1,39 +1,16 @@
 #pragma once
 
-#include <array>
-
 class InputManager
 {
 
 public:
 
-    static const int KEY_COUNT = 8;
-
     InputManager();
 
     void Update();
 
-    bool IsPressed(
+    bool IsLanePressed(
         int lane
     ) const;
-
-    bool IsJustPressed(
-        int lane
-    ) const;
-
-    bool IsJustReleased(
-        int lane
-    ) const;
-
-    void SetState(
-        int lane,
-        bool value
-    );
-
-private:
-
-    std::array<bool, KEY_COUNT> current;
-
-    std::array<bool, KEY_COUNT> previous;
 
 };

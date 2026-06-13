db3e9fc6e4a3f4061dc2079019c039908b1a2ac9
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-13 14:49:35 +0800

    Yea kinda looks like it

--- NativeEngine/Gameplay/NoteData.hpp
+++ NativeEngine/Gameplay/NoteData.hpp
@@ -5,25 +5,37 @@ struct NoteData
 
     float time;
 
-    int lane;
-
     float sustainLength;
+
+    int lane;
 
     bool mustHit;
 
     bool hit;
 
+    bool isHold;
+
+    bool holding;
+
+    bool completed;
+
     NoteData()
     {
         time = 0.0f;
 
-        lane = 0;
-
         sustainLength = 0.0f;
+
+        lane = 0;
 
         mustHit = true;
 
         hit = false;
+
+        isHold = false;
+
+        holding = false;
+
+        completed = false;
     }
 
 };

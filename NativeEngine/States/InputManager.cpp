7258c6f09a7e82bccb250fe5a92cc08afc1e0dbc
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-16 05:31:01 +0800

    Ok

--- NativeEngine/States/InputManager.cpp
+++ NativeEngine/States/InputManager.cpp
@@ -2,20 +2,37 @@
 
 InputManager::InputManager()
 {
-
+    for(int i = 0; i < 4; i++)
+    {
+        lanePressed[i] = false;
+    }
 }
 
 void InputManager::Update()
 {
+    // Future:
+    // Read keyboard
+    // Read touch
+    // Read gamepad
+}
 
+void InputManager::SetLaneState(
+    int lane,
+    bool pressed
+)
+{
+    if(lane < 0 || lane >= 4)
+        return;
+
+    lanePressed[lane] = pressed;
 }
 
 bool InputManager::IsLanePressed(
-int lane
+    int lane
 ) const
 {
-    // Future:
-    // Keyboard/Touch input
+    if(lane < 0 || lane >= 4)
+        return false;
 
-    return false;
+    return lanePressed[lane];
 }

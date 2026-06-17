c091cb54a2f1d86059a4c181f069c1f01395de28
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-17 13:16:14 +0800

    Ok that would do the  judge counter

--- NativeEngine/Gameplay/ScoreManager.cpp
+++ NativeEngine/Gameplay/ScoreManager.cpp
@@ -3,10 +3,14 @@
 ScoreManager::ScoreManager()
 {
     score = 0;
-
     combo = 0;
-
     misses = 0;
+
+    marvelousCount = 0;
+    sickCount = 0;
+    goodCount = 0;
+    badCount = 0;
+    missCount = 0;
 }
 
 void ScoreManager::AddJudge(
@@ -15,29 +19,34 @@ JudgeResult result)
     switch(result)
     {
         case JudgeResult::Marvelous:
-            score += 350;
-            combo++;
-            break;
-
+    score += 500;
+    combo++;
+    marvelousCount++;
+    break;
+        
         case JudgeResult::Sick:
-            score += 300;
-            combo++;
-            break;
-
+    score += 350;
+    combo++;
+    sickCount++;
+    break;
+        
         case JudgeResult::Good:
-            score += 200;
-            combo++;
-            break;
-
+    score += 200;
+    combo++;
+    goodCount++;
+    break;
+        
         case JudgeResult::Bad:
-            score += 50;
-            combo = 0;
-            break;
-
+    score += 50;
+    combo = 0;
+    badCount++;
+    break;
+        
         case JudgeResult::Miss:
-            combo = 0;
-            misses++;
-            break;
+    combo = 0;
+    misses++;
+    missCount++;
+    break;
     }
 }
 
@@ -53,5 +62,5 @@ int ScoreManager::GetCombo() const
 
 int ScoreManager::GetMisses() const
 {
-    return misses;
+    return misses && missCount;
 }

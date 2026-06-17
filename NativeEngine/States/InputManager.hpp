5f6a0a5d758f84e871fe1ae381314e39eb8b0c4f
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-17 08:01:30 +0800

    Needing the pressed, hold, release of the note

--- NativeEngine/States/InputManager.hpp
+++ NativeEngine/States/InputManager.hpp
@@ -2,24 +2,25 @@
 
 class InputManager
 {
-
 public:
 
     InputManager();
 
     void Update();
 
-    void SetLaneState(
-        int lane,
-        bool pressed
-    );
+    // Lane control (from backend input system)
+    void SetLaneState(int lane, bool down);
 
-    bool IsLanePressed(
-        int lane
-    ) const;
+    // Queries
+    bool IsLanePressed(int lane) const;   // just pressed this frame
+    bool IsLaneHeld(int lane) const;      // currently held
+    bool IsLaneReleased(int lane) const;  // just released
 
 private:
 
-    bool lanePressed[4];
+    bool current[4];
+    bool previous[4];
 
+    bool pressed[4];
+    bool released[4];
 };

715357a2c4a0107bdc95380b6936ac1a5a5e6d27
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-19 04:58:17 +0800

    Yea it is read

--- NativeEngine/Loader/SongLoader.cpp
+++ NativeEngine/Loader/SongLoader.cpp
@@ -10,15 +10,39 @@ const std::string& song)
 {
     currentSong = song;
 
-    // Future:
-    // Locate song folder
-    // Load instrumental
-    // Load voices
-
     return true;
 }
 
 std::string SongLoader::GetSongPath() const
 {
     return currentSong;
+}
+
+std::string SongLoader::GetSongFolder() const
+{
+    return
+        "Assets/BaseGame/Songs/" +
+        currentSong +
+        "/";
+}
+
+std::string SongLoader::GetChartPath() const
+{
+    return
+        GetSongFolder() +
+        "chart.json";
+}
+
+std::string SongLoader::GetMetadataPath() const
+{
+    return
+        GetSongFolder() +
+        "song.json";
+}
+
+std::string SongLoader::GetEventsPath() const
+{
+    return
+        GetSongFolder() +
+        "events.json";
 }

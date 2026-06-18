a3e868b46fc69356f47cff8aa8ffc4028a6cba99
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-18 20:46:27 +0800

    Yea this is a part from the og fnf to get the Mt data part

---
+++ NativeEngine/Loader/SongMetadata.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+
+class SongMetadata
+{
+
+public:
+
+    std::string name;
+
+    float bpm;
+
+    float speed;
+
+    std::string player;
+
+    std::string opponent;
+
+    std::string stage;
+
+};

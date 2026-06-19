c2a96c3acbc5300fabbf790c4dfbdc9f0873bbdf
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-19 17:03:09 +0800

    That should do

--- NativeEngine/States/Editor/ChartEditorState.cpp
+++ NativeEngine/States/Editor/ChartEditorState.cpp
@@ -1,12 +1,38 @@
 #include "ChartEditorState.hpp"
 
+#include "../../Loader/SongLoader.hpp"
+#include "../../States/ChartParser.hpp"
+#include "../../Gameplay/NoteManager.hpp"
+
 ChartEditorState::ChartEditorState()
 {
-
+    songLoader = nullptr;
+    chartParser = nullptr;
+    noteManager = nullptr;
 }
 
 bool ChartEditorState::Initialize()
 {
+    songLoader = new SongLoader();
+
+    chartParser = new ChartParser();
+
+    noteManager = new NoteManager();
+
+    songLoader->Load(
+        "Tutorial"
+    );
+
+    chartParser->Parse(
+        songLoader
+        ->GetChartPath()
+        .c_str()
+    );
+
+    noteManager->LoadNotes(
+        chartParser->GetNotes()
+    );
+
     return true;
 }
 
@@ -18,10 +44,17 @@ float deltaTime)
 
 void ChartEditorState::Render()
 {
-
+    // Future:
+    // Draw grid
+    // Draw receptors
+    // Draw notes
 }
 
 void ChartEditorState::Shutdown()
 {
+    delete noteManager;
 
+    delete chartParser;
+
+    delete songLoader;
 }

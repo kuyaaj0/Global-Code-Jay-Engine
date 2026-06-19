90e091643c593b33b946e08691899df7db601645
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-19 16:58:56 +0800

    Add ChartEditorState class with basic methods
    Implement basic structure for ChartEditorState class with initialization, update, render, and shutdown methods.

---
+++ NativeEngine/States/Editor/ChartEditorState.cpp
@@ -0,0 +1,27 @@
+#include "ChartEditorState.hpp"
+
+ChartEditorState::ChartEditorState()
+{
+
+}
+
+bool ChartEditorState::Initialize()
+{
+    return true;
+}
+
+void ChartEditorState::Update(
+float deltaTime)
+{
+
+}
+
+void ChartEditorState::Render()
+{
+
+}
+
+void ChartEditorState::Shutdown()
+{
+
+}

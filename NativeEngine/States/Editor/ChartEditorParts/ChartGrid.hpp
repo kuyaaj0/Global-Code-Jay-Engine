13d14aff112e487d19d50e6e0281ee6d19dec148
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-19 17:09:06 +0800

    This would do to fill

--- NativeEngine/States/Editor/ChartEditorParts/ChartGrid.hpp
+++ NativeEngine/States/Editor/ChartEditorParts/ChartGrid.hpp
@@ -1,1 +1,26 @@
-//still empty yet
+#pragma once
+
+class Renderer;
+
+class ChartGrid
+{
+
+public:
+
+    ChartGrid();
+
+    void Update();
+
+    void Render(
+        Renderer* renderer
+    );
+
+private:
+
+    int laneCount;
+
+    float laneWidth;
+
+    float beatSpacing;
+
+};

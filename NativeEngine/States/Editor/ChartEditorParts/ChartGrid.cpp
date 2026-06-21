0dfcb052918d8d672c4c0397395c277135c8da34
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-21 08:47:48 +0800

    I guess this would be the grid part

--- NativeEngine/States/Editor/ChartEditorParts/ChartGrid.cpp
+++ NativeEngine/States/Editor/ChartEditorParts/ChartGrid.cpp
@@ -1,6 +1,8 @@
 #include "ChartGrid.hpp"
 
 #include "../../../Renderer/Renderer.hpp"
+#include "../../../Math/Matrix4.hpp"
+#include "../../../Math/Vector4.hpp"
 
 ChartGrid::ChartGrid()
 {
@@ -9,6 +11,8 @@ ChartGrid::ChartGrid()
     laneWidth = 120.0f;
 
     beatSpacing = 64.0f;
+
+    visibleBeats = 32;
 }
 
 void ChartGrid::Update()
@@ -22,9 +26,47 @@ Renderer* renderer)
     if(renderer == nullptr)
         return;
 
-    // Future:
-    // Draw lane lines
-    // Draw beat lines
-    // Draw section lines
-    // Draw receptors
+    float gridHeight =
+        visibleBeats *
+        beatSpacing;
+
+    for(int lane = 0;
+        lane < laneCount;
+        lane++)
+    {
+        float x =
+            (lane - 1.5f) *
+            laneWidth;
+
+        Matrix4 world =
+            Matrix4::Translation(
+                x,
+                0.0f,
+                0.0f
+            ) *
+            Matrix4::Scale(
+                laneWidth,
+                gridHeight,
+                1.0f
+            );
+
+        Vector4 quad[4];
+
+        quad[0] =
+            Vector4(-0.5f,-0.5f,0.0f,1.0f);
+
+        quad[1] =
+            Vector4( 0.5f,-0.5f,0.0f,1.0f);
+
+        quad[2] =
+            Vector4( 0.5f, 0.5f,0.0f,1.0f);
+
+        quad[3] =
+            Vector4(-0.5f, 0.5f,0.0f,1.0f);
+
+        renderer->DrawQuad(
+            world,
+            quad
+        );
+    }
 }

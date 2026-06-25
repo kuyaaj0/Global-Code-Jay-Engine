9f7ffe2fdd256ba2a28c74c6251f3bf43bdf947c
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-25 11:36:27 +0800

    I guess this was the only thing it needs

--- GameTurboApp/src/main/java/MainActivity.java
+++ GameTurboApp/src/main/java/MainActivity.java
@@ -3,6 +3,7 @@ package com.game.gameturbo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;

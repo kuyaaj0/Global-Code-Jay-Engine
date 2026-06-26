325d04ebacd9d6d53e3ce83b5191eccc413a4f2b
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-26 16:06:00 +0800

    Ok that should do

---
+++ NativeEngine/States/StateManager.cpp
@@ -0,0 +1,43 @@
+#include "StateManager.hpp"
+
+StateManager::StateManager()
+{
+    currentState = nullptr;
+}
+
+StateManager::~StateManager()
+{
+    if(currentState != nullptr)
+    {
+        currentState->Shutdown();
+        delete currentState;
+    }
+}
+
+void StateManager::ChangeState(State* nextState)
+{
+    if(currentState != nullptr)
+    {
+        currentState->Shutdown();
+        delete currentState;
+    }
+
+    currentState = nextState;
+
+    if(currentState != nullptr)
+    {
+        currentState->Initialize();
+    }
+}
+
+void StateManager::Update(float deltaTime)
+{
+    if(currentState != nullptr)
+        currentState->Update(deltaTime);
+}
+
+void StateManager::Render(Renderer* renderer)
+{
+    if(currentState != nullptr)
+        currentState->Render(renderer);
+}

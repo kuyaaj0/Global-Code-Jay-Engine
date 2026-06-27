d3b932e263b4b23eb3aa3abaef0cabefb51dc58d
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-27 11:31:05 +0800

    Ok that's should be enough

---
+++ NativeEngine/Gameplay/CharacterData.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+
+struct CharacterData
+{
+    // Basic Info
+    std::string name;
+    std::string displayName;
+    std::string folder;
+
+    // Graphics
+    std::string spriteAtlas;
+    std::string spriteXML;
+    std::string spriteJSON;
+    std::string animationFile;
+    std::string icon;
+
+    // Character Configuration
+    std::string offsets;
+    std::string healthColor;
+
+    // Gameplay
+    bool playerCharacter;
+    bool flipX;
+    bool flipY;
+
+    // Default Constructor
+    CharacterData()
+    {
+        name = "";
+        displayName = "";
+        folder = "";
+
+        spriteAtlas = "";
+        spriteXML = "";
+        spriteJSON = "";
+        animationFile = "";
+        icon = "";
+
+        offsets = "";
+        healthColor = "#FFFFFF";
+
+        playerCharacter = false;
+
+        flipX = false;
+        flipY = false;
+    }
+};

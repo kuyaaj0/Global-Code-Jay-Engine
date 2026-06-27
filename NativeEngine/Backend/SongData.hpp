f2602db1e13f6ef7d050d7d2a51f6189bc8255f7
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-27 10:49:29 +0800

    Ok I guess this might be important

---
+++ NativeEngine/Backend/SongData.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+struct SongData
+{
+    std::string name;
+
+    std::string folder;
+
+    std::string chart;
+
+    std::string instrumental;
+
+    std::string voices;
+
+    bool hasVoices;
+};

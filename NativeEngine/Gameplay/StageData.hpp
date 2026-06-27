e069b119bc4e4f883616195cdc46c8901e52dbd1
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-27 19:25:40 +0800

    Updated parts

--- NativeEngine/Gameplay/StageData.hpp
+++ NativeEngine/Gameplay/StageData.hpp
@@ -1,16 +1,37 @@
+#pragma once
+
+#include <string>
+
 struct StageData
 {
+    // Basic Information
     std::string name;
-
+    std::string displayName;
     std::string folder;
 
+    // Stage Assets
     std::string background;
-
     std::string foreground;
-
     std::string props;
 
-    float defaultZoom;
+    // Stage Configuration
+    float defaultCameraZoom;
 
     bool pixelStage;
+
+    // Constructor
+    StageData()
+    {
+        name = "";
+        displayName = "";
+        folder = "";
+
+        background = "";
+        foreground = "";
+        props = "";
+
+        defaultCameraZoom = 1.0f;
+
+        pixelStage = false;
+    }
 };

1a553f32134b4faf27d84a241f6a1539169b056c
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-27 19:23:59 +0800

    Ok that would be next for stage data

---
+++ NativeEngine/Gameplay/StageData.hpp
@@ -0,0 +1,16 @@
+struct StageData
+{
+    std::string name;
+
+    std::string folder;
+
+    std::string background;
+
+    std::string foreground;
+
+    std::string props;
+
+    float defaultZoom;
+
+    bool pixelStage;
+};

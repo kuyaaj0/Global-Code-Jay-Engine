c4f0de648317054bd5a6dd17052c347b3a9f38c5
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-27 10:28:46 +0800

    Bruh why two

---
+++ NativeEngine/States/StoryMenuState.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "State.hpp"
+
+class Renderer;
+
+class StoryMenuState : public State
+{
+public:
+
+    StoryMenuState();
+
+    bool Initialize() override;
+
+    void Update(float deltaTime) override;
+
+    void Render(Renderer* renderer) override;
+
+    void Shutdown() override;
+
+private:
+
+    void DrawMenu();
+
+    void MoveUp();
+
+    void MoveDown();
+
+    void ToggleStorySource();
+
+    void SelectWeek();
+
+    bool useJayStory;
+
+    int selectedWeek;
+
+    float inputCooldown;
+
+    static constexpr int JAY_CHAPTER_COUNT = 3;
+
+    static constexpr int FNF_WEEK_COUNT = 7;
+
+    const char* jayStory[JAY_CHAPTER_COUNT]
+    {
+        "Chapter 1",
+        "Chapter 2",
+        "Chapter 3"
+    };
+
+    const char* fnfWeeks[FNF_WEEK_COUNT]
+    {
+        "Week 1",
+        "Week 2",
+        "Week 3",
+        "Week 4",
+        "Week 5",
+        "Week 6",
+        "Week 7"
+    };
+};

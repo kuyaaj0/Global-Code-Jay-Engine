4f8aae69bd6b9d28de9e36fc4cf721bcf6a097d2
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-27 10:26:15 +0800

    Ok next part

--- NativeEngine/States/MainMenuState.cpp
+++ NativeEngine/States/MainMenuState.cpp
@@ -71,6 +71,52 @@ void MainMenuState::DrawMenu()
     std::cout << std::endl;
 }
 
+void MainMenuState::MoveUp()
+{
+    selectedOption--;
+
+    if(selectedOption < 0)
+        selectedOption = MENU_COUNT - 1;
+
+    DrawMenu();
+}
+
+void MainMenuState::MoveDown()
+{
+    selectedOption++;
+
+    if(selectedOption >= MENU_COUNT)
+        selectedOption = 0;
+
+    DrawMenu();
+}
+
+void MainMenuState::SelectOption()
+{
+    switch(selectedOption)
+    {
+        case 0:
+            std::cout << "[MainMenu] Story Mode Selected." << std::endl;
+            break;
+
+        case 1:
+            std::cout << "[MainMenu] Freeplay Selected." << std::endl;
+            break;
+
+        case 2:
+            std::cout << "[MainMenu] Options Selected." << std::endl;
+            break;
+
+        case 3:
+            std::cout << "[MainMenu] Credits Selected." << std::endl;
+            break;
+
+        case 4:
+            std::cout << "[MainMenu] Exit Selected." << std::endl;
+            break;
+    }
+}
+
 void MainMenuState::Shutdown()
 {
     std::cout << "[MainMenu] Shutdown." << std::endl;

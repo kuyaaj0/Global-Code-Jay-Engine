eb85587e741b18ae91cc273fba1bad3d6b2e59fe
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-28 09:36:51 +0800

    This would help it from clicking and select below or up on the menu selection on the main menu state to StoryMenuState and Freeplay

--- NativeEngine/States/InputManager.cpp
+++ NativeEngine/States/InputManager.cpp
@@ -9,6 +9,20 @@ InputManager::InputManager()
         pressed[i] = false;
         released[i] = false;
     }
+
+    upCurrent = upPrevious = upPressed = false;
+
+    downCurrent = downPrevious = downPressed = false;
+
+    leftCurrent = leftPrevious = leftPressed = false;
+
+    rightCurrent = rightPrevious = rightPressed = false;
+
+    confirmCurrent = confirmPrevious = confirmPressed = false;
+
+    backCurrent = backPrevious = backPressed = false;
+
+    pauseCurrent = pausePrevious = pausePressed = false;
 }
 
 void InputManager::SetLaneState(int lane, bool down)
@@ -43,6 +57,78 @@ bool InputManager::IsLaneReleased(int lane) const
     return released[lane];
 }
 
+void InputManager::SetUpState(bool down)
+{
+    upCurrent = down;
+}
+
+void InputManager::SetDownState(bool down)
+{
+    downCurrent = down;
+}
+
+void InputManager::SetLeftState(bool down)
+{
+    leftCurrent = down;
+}
+
+void InputManager::SetRightState(bool down)
+{
+    rightCurrent = down;
+}
+
+void InputManager::SetConfirmState(bool down)
+{
+    confirmCurrent = down;
+}
+
+void InputManager::SetBackState(bool down)
+{
+    backCurrent = down;
+}
+
+void InputManager::SetPauseState(bool down)
+{
+    pauseCurrent = down;
+}
+
+//query function to press
+
+bool InputManager::IsUpPressed() const
+{
+    return upPressed;
+}
+
+bool InputManager::IsDownPressed() const
+{
+    return downPressed;
+}
+
+bool InputManager::IsLeftPressed() const
+{
+    return leftPressed;
+}
+
+bool InputManager::IsRightPressed() const
+{
+    return rightPressed;
+}
+
+bool InputManager::IsConfirmPressed() const
+{
+    return confirmPressed;
+}
+
+bool InputManager::IsBackPressed() const
+{
+    return backPressed;
+}
+
+bool InputManager::IsPausePressed() const
+{
+    return pausePressed;
+}
+
 void InputManager::Update()
 {
     for(int i = 0; i < 4; i++)
@@ -56,4 +142,26 @@ void InputManager::Update()
         // save state
         previous[i] = current[i];
     }
+
+    // Navigation
+
+    upPressed = upCurrent && !upPrevious;
+    upPrevious = upCurrent;
+
+    downPressed = downCurrent && !downPrevious;
+    downPrevious = downCurrent;
+
+    leftPressed = leftCurrent && !leftPrevious;
+    leftPrevious = leftCurrent;
+
+    rightPressed = rightCurrent && !rightPrevious;
+    rightPrevious = rightCurrent;
+
+    confirmPressed = confirmCurrent && !confirmPrevious;
+    confirmPrevious = confirmCurrent;
+    backPressed = backCurrent && !backPrevious;
+    backPrevious = backCurrent;
+
+    pausePressed = pauseCurrent && !pausePrevious;
+    pausePrevious = pauseCurrent;
 }

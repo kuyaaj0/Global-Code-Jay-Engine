3ee3b87f951b9c2baa0244f56348ada0974affa0
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-28 09:17:12 +0800

    Ok that should do

--- NativeEngine/States/InputManager.hpp
+++ NativeEngine/States/InputManager.hpp
@@ -16,6 +16,28 @@ public:
     bool IsLaneHeld(int lane) const;      // currently held
     bool IsLaneReleased(int lane) const;  // just released
 
+    // ----------------------
+    // Engine Navigation
+    // ----------------------
+
+    void SetUpState(bool down);
+    void SetDownState(bool down);
+    void SetLeftState(bool down);
+    void SetRightState(bool down);
+
+    void SetConfirmState(bool down);
+    void SetBackState(bool down);
+    void SetPauseState(bool down);
+
+    bool IsUpPressed() const;
+    bool IsDownPressed() const;
+    bool IsLeftPressed() const;
+    bool IsRightPressed() const;
+
+    bool IsConfirmPressed() const;
+    bool IsBackPressed() const;
+    bool IsPausePressed() const;
+
 private:
 
     bool current[4];
@@ -23,4 +45,34 @@ private:
 
     bool pressed[4];
     bool released[4];
+
+    // Navigation
+
+    bool upCurrent;
+    bool upPrevious;
+    bool upPressed;
+
+    bool downCurrent;
+    bool downPrevious;
+    bool downPressed;
+
+    bool leftCurrent;
+    bool leftPrevious;
+    bool leftPressed;
+
+    bool rightCurrent;
+    bool rightPrevious;
+    bool rightPressed;
+
+    bool confirmCurrent;
+    bool confirmPrevious;
+    bool confirmPressed;
+
+    bool backCurrent;
+    bool backPrevious;
+    bool backPressed;
+
+    bool pauseCurrent;
+    bool pausePrevious;
+    bool pausePressed;
 };

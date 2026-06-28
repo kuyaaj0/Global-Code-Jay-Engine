906e8e0cbd7192c53f4ebca2a93f78ad085ee98c
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-28 09:47:17 +0800

    Add this part to prevent a new one and do this for access to other file

--- NativeEngine/States/State.hpp
+++ NativeEngine/States/State.hpp
@@ -18,6 +18,11 @@ public:
         stateManager = manager;
     }
 
+    void SetInputManager(InputManager* input)
+    {
+        inputManager = input;
+    }
+
     virtual void Render(Renderer* renderer) = 0;
 
     virtual void Shutdown() = 0;
@@ -25,4 +30,6 @@ public:
 protected:
 
     StateManager* stateManager;
+
+    InputManager* inputManager;
 };

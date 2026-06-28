952a2b8b21adcf727a86a76884aa91a62d688b05
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-06-28 10:41:23 +0800

    For week data to get for Json weeks strings

---
+++ NativeEngine/Gameplay/WeekData.hpp
@@ -0,0 +1,14 @@
+struct WeekData
+{
+    std::string weekName;
+
+    std::string songs[3];
+
+    std::string boyfriend;
+
+    std::string girlfriend;
+
+    std::string opponent;
+
+    std::string stage;
+};

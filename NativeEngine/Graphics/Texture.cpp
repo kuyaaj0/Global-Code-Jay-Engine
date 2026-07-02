6c923f6d07ac19dc8646b7eb41f47b9d87f53878
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-07-02 21:34:24 +0800

    Add frame size handling to Texture class
    Added frame size properties and methods for texture management.

--- NativeEngine/Graphics/Texture.cpp
+++ NativeEngine/Graphics/Texture.cpp
@@ -5,6 +5,8 @@ Texture::Texture()
     id = 0;
     width = 0;
     height = 0;
+    frameWidth = 0;
+    frameHeight = 0;
 }
 
 bool Texture::Load(
@@ -15,7 +17,14 @@ const std::string& path)
 
     id = 1;
 
+    this->path = path;
+
     return true;
+}
+
+const std::string& Texture::GetPath() const
+{
+    return path;
 }
 
 void Texture::Unload()
@@ -31,4 +40,38 @@ int Texture::GetWidth() const
 int Texture::GetHeight() const
 {
     return height;
+}
+
+void Texture::SetFrameSize(
+int width,
+int height)
+{
+    frameWidth = width;
+    frameHeight = height;
+}
+
+int Texture::GetFrameWidth() const
+{
+    return frameWidth;
+}
+
+int Texture::GetFrameHeight() const
+{
+    return frameHeight;
+}
+
+int Texture::GetColumns() const
+{
+    if(frameWidth == 0)
+        return 0;
+
+    return width / frameWidth;
+}
+
+int Texture::GetRows() const
+{
+    if(frameHeight == 0)
+        return 0;
+
+    return height / frameHeight;
 }

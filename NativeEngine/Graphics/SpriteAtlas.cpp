aaba00c47ee61aa3d4f029134967c2a68deeb444
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-07-03 08:20:52 +0800

    Add the others as well

--- NativeEngine/Graphics/SpriteAtlas.cpp
+++ NativeEngine/Graphics/SpriteAtlas.cpp
@@ -1,14 +1,36 @@
 #include "SpriteAtlas.hpp"
 
+#include <fstream>
+#include <sstream>
+
 SpriteAtlas::SpriteAtlas()
 {
-
+    
 }
 
 bool SpriteAtlas::Load(
 const std::string& xmlFile)
 {
-    // XML parser will go here
+    Clear();
+
+    std::ifstream file(xmlFile);
+
+    if(!file.is_open())
+        return false;
+
+    std::string line;
+
+    while(std::getline(file, line))
+    {
+        AtlasFrame frame;
+
+        if(ParseLine(line, frame))
+        {
+            frames[frame.name] = frame;
+        }
+    }
+
+    file.close();
 
     return true;
 }
@@ -44,6 +66,94 @@ AtlasFrame
 size_t SpriteAtlas::GetFrameCount() const
 {
     return frames.size();
+}
+
+std::string SpriteAtlas::GetAttribute(
+    const std::string& line,
+    const std::string& attribute)
+{
+    std::string key =
+        attribute + "=\"";
+
+    size_t start =
+        line.find(key);
+
+    if(start == std::string::npos)
+        return "";
+
+    start += key.length();
+
+    size_t end =
+        line.find("\"", start);
+
+    if(end == std::string::npos)
+        return "";
+
+    return line.substr(
+        start,
+        end - start
+    );
+}
+
+bool SpriteAtlas::ParseLine(
+const std::string& line,
+AtlasFrame& frame)
+{
+    if(line.find("<SubTexture") == std::string::npos)
+        return false;
+
+    frame.name =
+        GetAttribute(line, "name");
+
+    frame.x =
+        std::stoi(
+            GetAttribute(line, "x")
+        );
+
+    frame.y =
+        std::stoi(
+            GetAttribute(line, "y")
+        );
+
+    frame.width =
+        std::stoi(
+            GetAttribute(line, "width")
+        );
+
+    frame.height =
+        std::stoi(
+            GetAttribute(line, "height")
+        );
+
+    std::string value;
+
+    value =
+        GetAttribute(line, "frameX");
+
+    if(!value.empty())
+        frame.frameX = std::stoi(value);
+
+    value =
+        GetAttribute(line, "frameY");
+
+    if(!value.empty())
+        frame.frameY = std::stoi(value);
+
+    value =
+        GetAttribute(line, "frameWidth");
+
+    if(!value.empty())
+        frame.frameWidth =
+            std::stoi(value);
+
+    value =
+        GetAttribute(line, "frameHeight");
+
+    if(!value.empty())
+        frame.frameHeight =
+            std::stoi(value);
+
+    return true;
 }
 
 void SpriteAtlas::Clear()
